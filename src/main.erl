d6d9cb41bc404f849ed536601dadc194e2c82ea1
Akhil <akhil@Akhils-MacBook-Pro.local>
2022-09-05 23:46:37 -0400

    initial commit

---
+++ src/main.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author akhil
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 05. Sept 2022 11:43 PM
+%%%-------------------------------------------------------------------
+-module(main).
+-author("akhil").
+
+%% API
+-export([hello_world/0]).
+
+hello_world() -> io:fwrite("hello world").
