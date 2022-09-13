5027d1c9bc0a3b7b844aff45d1c069c94c08f596
Akhil <akhil@Akhils-MacBook-Pro.local>
2022-09-13 00:27:49 -0400

    skeleton code with basic functions.

--- src/main.erl
+++ src/main.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %%% @author akhil
-%%% @copyright (C) 2022, <COMPANY>
+%%% @copyright (C) 2022, <UF>
 %%% @doc
 %%%
 %%% @end
@@ -9,7 +9,30 @@
 -module(main).
 -author("akhil").
 
-%% API
--export([hello_world/0]).
+-define( REQUIRED_PROCESSES, 10).
+-define( UFID,"akhilsrivatsa;").
 
-hello_world() -> io:fwrite("hello world").
+-import(string,[concat/2]).
+-export([start/0, generateHash/1]).
+
+start() ->
+  {ok, LeadingZeros} = io:read("Enter the required no.of leading zeros: \n"),
+  startMining(1, LeadingZeros).
+
+generateRandomString() -> base64:encode(crypto:strong_rand_bytes(8)).  % re:replace(base64:encode(crypto:strong_rand_bytes(8)),"\\W","",[global,{return,binary}]),
+
+%checkLeadingZeros(HashedString, LeadingZeroes, idx) ->
+ % io:format("Leading Zeroes function called \n").
+
+generateHash(LeadingZeros) ->
+  RandomString = generateRandomString(),
+  HashInputString = concat([?UFID],RandomString) ,
+  HashOutputString  = binary:decode_unsigned(crypto:hash(sha256 , HashInputString)),
+  io:format("~64.16.0b \n", [HashOutputString]).
+  %generateHash(LeadingZeros).  Used for recursively mining coins. Commen
+
+startMining(2,LeadingZeros) -> {ok, LeadingZeros};
+
+startMining(Itr, LeadingZeros) ->
+  spawn( fun() -> generateHash(LeadingZeros) end),
+  startMining(Itr + 1, LeadingZeros).
